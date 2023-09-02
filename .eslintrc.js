0dda875b1e8bae3b67fdbb01a96e9e821453a9ad
omazoz <oumaymamazoz12@gmail.com>
2023-09-02 13:23:41 +0100

    update

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Specify your ESLint configuration here.
+  // For example, you can extend a popular preset, such as "eslint:recommended":
+  extends: 'eslint:recommended',
+  // You can also add or override rules:
+  rules: {
+    // Customize ESLint rules here
+    'no-unused-vars': 'warn', // Treat unused variables as warnings
+  },
+};
+
